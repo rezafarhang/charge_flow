21354fb61a76230ee65c98996f5c6c17834cc769
Reza Farhang <rezafarhangoa@gmail.com>
2025-12-19 12:07:05 +0330

    Add approve credit request endpoint path

--- apps/transaction/urls.py
+++ apps/transaction/urls.py
@@ -4,4 +4,5 @@ from apps.transaction import views
 
 urlpatterns = [
     path('credit-request/', views.CreateCreditRequestView.as_view(), name='create_credit_request'),
+    path('approve/', views.ApproveCreditRequestView.as_view(), name='approve_credit_request'),
 ]
