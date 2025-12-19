c7c9262752ad5156b302ba1f478985d9c07d3d86
Reza Farhang <rezafarhangoa@gmail.com>
2025-12-19 11:44:48 +0330

    Add credit request endpoint

---
+++ apps/transaction/views.py
@@ -0,0 +1,18 @@
+from rest_framework import status, permissions, views, response
+
+from apps.transaction import services, serializers, consts, models
+
+
+class CreateCreditRequestView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated,]
+
+    def post(self, request):
+        serializer = serializers.CreateCreditRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        txn = services.CreditRequestService.create_credit_request(
+            user=request.user,
+            amount=serializer.validated_data.get('amount')
+        )
+        response_serializer = serializers.TransactionSerializer(txn)
+        return response.Response(response_serializer.data, status=status.HTTP_201_CREATED)
