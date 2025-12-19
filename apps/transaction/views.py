bc38406606da1caef36cc3dc52d0a660e283d30f
Reza Farhang <rezafarhangoa@gmail.com>
2025-12-19 12:07:15 +0330

    Add approve credit request endpoint

--- apps/transaction/views.py
+++ apps/transaction/views.py
@@ -16,3 +16,18 @@ class CreateCreditRequestView(views.APIView):
         )
         response_serializer = serializers.TransactionSerializer(txn)
         return response.Response(response_serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ApproveCreditRequestView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated,]
+
+    def post(self, request):
+        serializer = serializers.ProcessTransactionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        txn = services.CreditRequestService.approve_credit_request(
+            transaction_id=serializer.validated_data.get('transaction_id'),
+            admin_user=request.user
+        )
+        response_serializer = serializers.TransactionSerializer(txn)
+        return response.Response(response_serializer.data, status=status.HTTP_200_OK)
