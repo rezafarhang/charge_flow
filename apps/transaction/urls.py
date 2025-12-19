d4908d5327aa6b5b52fe58557a0cac0925f84b41
Reza Farhang <rezafarhangoa@gmail.com>
2025-12-19 17:09:19 +0330

    Add sell charge endpoint path

--- apps/transaction/urls.py
+++ apps/transaction/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('wallet/', views.WalletBalanceView.as_view(), name='wallet_balance'),
     path('credit-request/', views.CreateCreditRequestView.as_view(), name='create_credit_request'),
     path('status/', views.UpdateCreditRequestView.as_view(), name='update_credit_request_status'),
+    path('sell-charge/', views.SellChargeView.as_view(), name='sell_charge'),
 ]
